b64c34f71d37c097f5cb923049b0ed8e4db1d8a9
Yury Sh <yignor@gmail.com>
2020-12-07 15:53:42 +0300

    add login/reg

---
+++ public/loginPage.js
@@ -0,0 +1,9 @@
+'use strick';
+
+const { response } = require("express");
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = data => ApiConnector.login(data, response => !response.success ? userForm.setLoginErrorMessage(response.error) : location.reload());
+
+userForm.registerFormCallback = data => ApiConnector.register(data, response => !response.success ? userForm.setRegisterErrorMessage(response.error) : location.reload());
