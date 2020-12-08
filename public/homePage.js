470a2e23fe144081761ddd8120985c933d94abe4
Yury Sh <yignor@gmail.com>
2020-12-08 16:12:11 +0300

    Continue

--- public/homePage.js
+++ public/homePage.js
@@ -1,1 +1,8 @@
 'use strick';
+
+const userLogout = new LogoutButton();
+
+userLogout.action = () => ApiConnector.logout(response => response.success ? location.reload() : console.log('Error'))
+
+const userRatesBoard = new RatesBoard();
+
