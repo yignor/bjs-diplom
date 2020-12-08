045f6e178ec8e8ec436ac758254a4d407d846e85
Yury Sh <yignor@gmail.com>
2020-12-08 18:16:01 +0300

    add some text

--- public/homePage.js
+++ public/homePage.js
@@ -4,5 +4,8 @@ const userLogout = new LogoutButton();
 
 userLogout.action = () => ApiConnector.logout(response => response.success ? location.reload() : console.log('Error'))
 
+ApiConnector.current(response => {response.success ? ProfileWidget.showProfile(response.data) : console.log('Error')}
+);
+
 const userRatesBoard = new RatesBoard();
 
